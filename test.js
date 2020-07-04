e4dd9a04f7e15c40a54c6694f61415839bbd55d1
lbugasu <lbugasu@gmail.com>
2020-07-04 17:43:06 -0400

    some progress

--- test.js
+++ test.js
@@ -1,35 +1,147 @@
 const fs = require('fs');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
-const log = require('lighthouse-logger');
 
-const flags = { onlyCategories: ['performance'] };
+const configJson = JSON.parse(fs.readFileSync("config.json"));
+// const flags = { onlyCategories: ['performance'] };
+
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+  return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+    // console.log("Launching lighthouse for " + url);
+    opts.lighthouseFlags.port = chrome.port;
+    return lighthouse(url, opts.lighthouseFlags, config).then(res => {
+      // console.log("Pa  rsing report for " + url);
+      return chrome.kill().then(() => res.report);
+    });
+  });
+}
+
+var content = "";
+try{
+  content = fs.readFileSync("websiteData.csv", 'utf8');
+}
+catch (e){
+  console.log('Error', e.stack)
+}
+let data = content.split("\n");
+
+let performanceOutput = "";
+
+data.splice(1,5).forEach(webInfo => {
+  let thisSite = webInfo
+  let parsedWebInfo = webInfo.split(',');
+  console.log(parsedWebInfo)
+  let website = "https://www." + parsedWebInfo[1];
+  // console.log("Starting analysis on " + website);
+  launchChromeAndRunLighthouse(website, configJson).then(results => {
+    const parsedResults = JSON.parse(results);
+    console.log("size: ",parsedResults["audits"]["total-byte-weight"]["displayValue"].split(" "))
+    let performance = parsedResults.categories.performance.id * 100;
+    let sizeString = parsedResults["audits"]["total-byte-weight"]["displayValue"].split(" ")
+
+    // let size = parseInt(sizeString[sizeString.length - 2])
+    // let multiple = sizeString[sizeString.length - 1]
+    // if (multiple === "KiB") {
+    //   size *= 1000;
+    // }
+    // else if (multiple === "MiB") {
+    //   size *= 1000000;
+    // }
+
+    // thisSite += ("," + performance + "," + size)
+    // performanceOutput += thisSite + "\n"
+    // const file = website.replace(/^https?:\/\//, "").replace(/[./]/g, "_") + "." + configJson.lighthouseFlags.output;
+    // const today = new Date();
+    // const folder = configJson.sortByDate ? configJson.writeTo + today.getFullYear() + "/" + (today.getMonth() + 1) + "/" : configJson.writeTo + file.replace("." + configJson.lighthouseFlags.output, "") + "/" + today.getFullYear() + "/" + (today.getMonth() + 1) + "/";
+    // const dest = folder + file;
 
 
-let data = [];
-fs.readFile("websiteData.csv", 'utf-8', (err, readData) => {
-  if (err) throw err;
-  data = readData;
+    /*
+    console.log("Writing analysis to " + dest);
+    fs.mkdir(folder, { recursive: true }, (err) => {
+      if (err) { console.log(err); }
+      else {
+        fs.writeFile(dest, results, (err) => {
+          if (err) { console.log(err); }
+          else { console.log("Analysis saved to " + dest) }
+        });
+      }
+    });
+    */
+  }).catch(err => console.log(err));
+});
 
-  fs.writeFile('Output.txt', performanceOutput, (err) => {
+/*
 
-    // In case of a error throw err. 
-    if (err) throw err;
-  })
-})
+var content = "";
+try{
+  content = fs.readFileSync("websiteData.csv", 'utf8');
+}
+catch (e){
+  console.log('Error', e.stack)
+}
 
-console.log(data)
-
-const parsedData = data.split("\n")
+const parsedData = content.split("\n")
 const performanceOutput = ""
 //Add performance and Size label
 const labels = parsedData[0] + ",Performance,Size on landing Page";
 
+
+// let thisSite = parsedData[1];
+
+  let webData = parsedData[1].split(',');
+  console.log(webData)
+  let website = "https://" + webData[1];
+parsedData.splice[2,3]
+parsedData.map(webInfo =>{
+  (async () => {
+    log.setLevel('info');
+    const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
+    const options = { output: 'json', onlyCategories: ['performance'], port: chrome.port };
+  
+    // let result = await lighthouse(website, options);
+    const runnerResult = await lighthouse(website, options);
+  
+    // `.report` is the HTML report as a string
+    const reportJSON = JSON.parse(runnerResult.report);
+    fs.writeFileSync('lhreport.json', reportJSON);
+    console.log(reportJSON["audits"]["total-byte-weight"]["displayValue"])
+    // `.lhr` is the Lighthouse Result as a JS object
+    console.log('Report is done for', runnerResult.lhr.finalUrl);
+    console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+  
+    await chrome.kill();
+  })();
+})
+(async () => {
+  log.setLevel('info');
+  const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
+  const options = { output: 'json', onlyCategories: ['performance'], port: chrome.port };
+
+  // let result = await lighthouse(website, options);
+  const runnerResult = await lighthouse(website, options);
+
+  // `.report` is the HTML report as a string
+  const reportJSON = JSON.parse(runnerResult.report);
+  fs.writeFileSync('lhreport.json', reportJSON);
+  console.log(reportJSON["audits"]["total-byte-weight"]["displayValue"])
+  // `.lhr` is the Lighthouse Result as a JS object
+  console.log('Report is done for', runnerResult.lhr.finalUrl);
+  console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+
+  await chrome.kill();
+})();
+
+
+
+ 
+
 for (let i = 1; i < 2; i++) {
   let thisSite = parsedData[i];
-  let webData = parsedData[i].split(',');
 
-  let website = "https://" + webData[2];
+  let webData = parsedData[i].split(',');
+  console.log(webData)
+  let website = "https://" + webData[1];
   (async () => {
     log.setLevel('info');
     const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
@@ -48,27 +160,9 @@ for (let i = 1; i < 2; i++) {
     let performance = result.lhr.categories.performance.score * 100;
     thisSite += ("," + performance + "," + size)
     performanceOutput += thisSite + "\n"
-    console.log(thisSite)
     await chrome.kill();
   })();
 
 }
 
-  // (async () => {
-  //   log.setLevel('info');
-  //   const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
-  //   const options = { output: 'json', onlyCategories: ['performance'], port: chrome.port };
-
-  //   // let result = await lighthouse(website, options);
-  //   const runnerResult = await lighthouse('https://motherfuckingwebsite.com', options);
-
-  //   // `.report` is the HTML report as a string
-  //   const reportJSON = JSON.parse(runnerResult.report);
-  //   fs.writeFileSync('lhreport.json', reportJSON);
-  //   console.log(reportJSON["audits"]["total-byte-weight"]["displayValue"])
-  //   // `.lhr` is the Lighthouse Result as a JS object
-  //   console.log('Report is done for', runnerResult.lhr.finalUrl);
-  //   console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
-
-  //   await chrome.kill();
-  // })();
+ */
