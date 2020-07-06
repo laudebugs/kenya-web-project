e3d67e35323487fbacc5830248171d10be2f0e81
Ubuntu <ubuntu@ip-172-31-14-81.us-east-2.compute.internal>
2020-07-06 20:13:25 +0000

    completed i30 to i60 reports

--- scripts/generateCompleteReports.js
+++ scripts/generateCompleteReports.js
@@ -9,7 +9,7 @@ try {
   console.log("Error", e.stack);
 }
 
-data.splice(0, 30).forEach((thisOne) => {
+data.splice(30, 60).forEach((thisOne) => {
   var address = "https://www." + thisOne.site;
   console.log("Starting analysis on " + address);
   lh.launchChromeAndRunLighthouse(address, configJson)
